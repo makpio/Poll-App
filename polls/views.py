98f8b0bb1b2e47ba6404082ae44be1d4d1e9b872
pmakaruk <makar1998@yahoo.pl>
2020-04-12 23:35:11 +0200

    add urls

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def index(requesrt):
-    return HttpResponse("my own website suuko")
+    return HttpResponse("my own website xddd")
