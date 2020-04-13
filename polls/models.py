0866b08edcda9ea4cac11597e372a59956923213
pmakaruk <makar1998@yahoo.pl>
2020-04-13 19:26:52 +0200

    add some dba conf

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,23 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Question(models.Model):
+    questionText = models.CharField(max_length=200)
+    pubDate = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.questionText
+    
+    def wasPublishedRecently(self):
+        return self.pubDate >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choiceText = models.CharField(max_length=200)
+    votes = models.ImageField(default = 0)
+
+    def __str__(self):
+        return self.choiceText
+
